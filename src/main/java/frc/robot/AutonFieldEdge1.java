9e256db6f8b54f20306797c931b53554544a939d
Team74 Scouter <team74.scouter@gmail.com>
2023-03-16 19:34:14 -0400

    Started 2 Piece Auton

--- src/main/java/frc/robot/AutonFieldEdge1.java
+++ src/main/java/frc/robot/AutonFieldEdge1.java
@@ -2,7 +2,7 @@ package frc.robot;
 
 public class AutonFieldEdge1 extends Auton{
 
-    public AutonFieldEdge1(DriveBase driveBase, Arm arm, Intake intake) {
+    public AutonFieldEdge1(DriveBase driveBase, Arm arm, Intake intake, boolean isBlue) {
         super(driveBase, arm, intake);
         //TODO Auto-generated constructor stub
     }
@@ -23,14 +23,26 @@ public class AutonFieldEdge1 extends Auton{
             arm.setShoulderPosition(85);
             intake.stopMotors();
         }else if(time <7.0){
-            driveBase.moveRobotFieldOriented(-0.5, 0.0, 0.0);
+            if(isBlue){
+                driveBase.moveRobotFieldOriented(0.5, 0.0, 0.0);
+            }else{
+                driveBase.moveRobotFieldOriented(-.5, 0.0, 0.0);
+            }
             arm.setShoulderPosition(0);
         }else if(driveBase.getGyroAngle() > 10 && driveBase.getGyroAngle() < 350 && time <14.0){
-            driveBase.moveRobotFieldOriented(0.1, 0.5, -100);
+            if(isBlue){
+                driveBase.moveRobotFieldOriented(-0.1, 0.5, 100);
+            }else{
+                driveBase.moveRobotFieldOriented(0.1, 0.5, 100);
+            }
             arm.setShoulderPosition(0);
             intake.intakeObject();
         }else if(time <14.5){
-            driveBase.moveRobotFieldOriented(0.08, 0.5, 0.0);
+            if(isBlue){
+                driveBase.moveRobotFieldOriented(-0.08, 0.5, 0.0);
+            }else{
+                driveBase.moveRobotFieldOriented(0.08, 0.5, 0.0);
+            }
             arm.setShoulderPosition(0);
             intake.intakeObject();
         }else{
--- src/main/java/frc/robot/AutonFieldEdge2.java
+++ src/main/java/frc/robot/AutonFieldEdge2.java
@@ -1,12 +1,86 @@
 package frc.robot;
 
-public class AutonFieldEdge2 extends Auton{
+import javax.swing.LayoutFocusTraversalPolicy;
 
-    public AutonFieldEdge2(DriveBase driveBase, Arm arm, Intake intake) {
-        super(driveBase, arm, intake);
+public class AutonFieldEdge2 extends Auton{
+    double lastSavedTime;
+    int i;
+    double rotation;
+
+    public AutonFieldEdge2(DriveBase driveBase, Arm arm, Intake intake, boolean isBlue) {
+        super(driveBase, arm, intake, isBlue);
         //TODO Auto-generated constructor stub
+        lastSavedTime = 0.0;
+        i = 0;
     }
     
     public void run(double time, double x){
+        if(time > 14.5){
+            i = 100;
+        }
+
+        if(i == 0){
+            driveBase.moveRobotFieldOriented(0, 0.0, 0);
+            arm.setShoulderPosition(80);
+            intake.stopMotors();
+
+            if(arm.getShoulderPosition() > 70){
+                i++;
+                lastSavedTime = time;
+            }
+        }else if(i == 1){
+            driveBase.moveRobotFieldOriented(0, -1.0, 0);
+            arm.setShoulderPosition(80);
+            intake.stopMotors();
+
+            if(time - lastSavedTime > 0.5){
+                i++;
+                lastSavedTime = time;
+            }
+        }else if(i == 2){
+            driveBase.moveRobotFieldOriented(0, 0.5, 0);
+            arm.setShoulderPosition(75);
+            intake.outtakeObject();
+
+            if(time - lastSavedTime > 0.5){
+                i++;
+                lastSavedTime = time;
+            }
+        }else if(i == 3){
+            if(isBlue){
+                driveBase.moveRobotFieldOriented(-1.0, 0.0, -100);
+            }else{
+                driveBase.moveRobotFieldOriented(1.0, 0.0, 100);
+            }
+            arm.setShoulderPosition(0);
+            intake.stopMotors();
+
+            if(time - lastSavedTime > 0.5){
+                i++;
+                lastSavedTime = time;
+            }
+        }else if(i == 4){
+            rotation = 100;
+            if(driveBase.getGyroAngle() > 10 && driveBase.getGyroAngle() < 350){
+                rotation = 0;
+            }
+
+            if(isBlue){
+                driveBase.moveRobotFieldOriented(0.0, 2.0, -rotation);
+            }else{
+                driveBase.moveRobotFieldOriented(0.0, 2.0, rotation);
+            }
+            arm.setShoulderPosition(0);
+            intake.stopMotors();
+
+            if(time - lastSavedTime > 1.0){
+                i++;
+                lastSavedTime = time;
+            }
+        }else if(i == 100){
+            driveBase.moveRobotFieldOriented(0, 0.0, 0);
+            arm.setShoulderPosition(arm.getShoulderPosition());
+            intake.stopMotors();
+        }
     }
 }
